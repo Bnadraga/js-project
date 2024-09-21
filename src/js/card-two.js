04020b9aa3d5a43a7b26d6a6cf59a40b0fadce2b
Bnadraga <player.gamer1mw@gmail.com>
2024-09-21 22:38:38 +0300

    fix

--- src/js/card-two.js
+++ src/js/card-two.js
@@ -1,30 +1,30 @@
-// const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
-// const btnTwo = document.getElementById('card-two-btn')
-// const textTwo = document.querySelector('.card-two__text')
+const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+const btnTwo = document.getElementById('card-two-btn')
+const textTwo = document.querySelector('.card-two__text')
 
-// btnTwo.addEventListener('click', () => {
-//     let inputTwo = document.getElementById('card-two-input').value
-//     inputTwo = parseInt(inputTwo)
+btnTwo.addEventListener('click', () => {
+    let inputTwo = document.getElementById('card-two-input').value
+    inputTwo = parseInt(inputTwo)
 
-//     if(inputTwo === randomNumberTwo){
-//         textTwo.innerHTML = `Вітаю, ви вгадали число ${randomNumberTwo}!`
-//     }
-//     else {
-//         textTwo.style.color = '#900'
-//         textTwo.innerHTML = `Ви програли, комп’ютер загадав ${randomNumberTwo}`
-//     }
-//     document.getElementById('card-two-input').value = '';
-// })
-const btnTwo = document.getElementById('card-two-btn');
-btnTwo.addEventLisener('click', () => {
-  const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
-  let inputTwo = document.getElementById('card-two-input').value;
-  inputTwo = parseInt(inputTwo);
-  if (inputTwo === randomNumberTwo) {
-    textTwo.innerHTML = `Вітаю, ви вгадали число ${randomNumberTwo}!`;
-  } else {
-    textTwo.style.color = '#900';
-    textTwo.innerHTML = `Ви програли, комп’ютер загадав ${randomNumberTwo}`;
-  }
-  document.getElementById('card-two-input').value = ''
-});
+    if(inputTwo === randomNumberTwo){
+        textTwo.innerHTML = `Вітаю, ви вгадали число ${randomNumberTwo}!`
+    }
+    else {
+        textTwo.style.color = '#900'
+        textTwo.innerHTML = `Ви програли, комп’ютер загадав ${randomNumberTwo}`
+    }
+    document.getElementById('card-two-input').value = '';
+})
+// const btnTwo = document.getElementById('card-two-btn');
+// btnTwo.addEventLisener('click', () => {
+//   const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+//   let inputTwo = document.getElementById('card-two-input').value;
+//   inputTwo = parseInt(inputTwo);
+//   if (inputTwo === randomNumberTwo) {
+//     textTwo.innerHTML = `Вітаю, ви вгадали число ${randomNumberTwo}!`;
+//   } else {
+//     textTwo.style.color = '#900';
+//     textTwo.innerHTML = `Ви програли, комп’ютер загадав ${randomNumberTwo}`;
+//   }
+//   document.getElementById('card-two-input').value = ''
+// });
