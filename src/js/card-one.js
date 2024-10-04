5ed25998b6be2d8458db8363bd0b902b63b2a977
Bnadraga <player.gamer1mw@gmail.com>
2024-10-04 20:15:01 +0300

    lesson

--- src/js/card-one.js
+++ src/js/card-one.js
@@ -1,11 +1,16 @@
 const birthYearInput = document.getElementById('birthYear');
 const checkButton = document.getElementById('checkButton');
 const resultElement = document.getElementById('result');
+const formOne = document.querySelector('.card-one__form');
+
+formOne.addEventListener('submit', e => {
+  e.preventDefault();
+});
 
 checkButton.addEventListener('click', function () {
   const year = parseInt(birthYearInput.value);
-
   let isLeapYear;
+
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     isLeapYear = true;
   } else {
